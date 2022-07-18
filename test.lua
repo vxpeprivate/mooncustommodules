00a8ce5796235696a185e879dd075a938fbd5354
vxpeprivate <108173427+vxpeprivate@users.noreply.github.com>
2022-07-18 21:07:59 +0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,53 @@
+repeat task.wait() until shared.GuiLibrary
+local GuiLibrary = shared.GuiLibrary
+local COB = function(tab, argstable) 
+    return GuiLibrary["ObjectsThatCanBeSaved"][tab.."Window"]["Api"].CreateOptionsButton(argstable)
+end
+
+COB("Utility", {
+    Name = "ClientCrasher",
+    Function = function(callback) 
+        if callback then
+            pcall(function() 
+                shared.VapeCrashAll() 
+            end) 
+        end
+    end,
+    HoverText = "crash all lol"
+})
+
+COB("Utility", {
+    Name = "ProjectileExploit",
+    Function = function(callback) 
+        if callback then
+            pcall(function() 
+                shared.VapeFireProjectile(game.Players:GetPlayers()) 
+            end) 
+        end
+    end,
+    HoverText = "tps projeciles at everyone"
+})
+
+COB("Utility", {
+    Name = "FunnyExploit",
+    Function = function(callback) 
+        if callback then
+            pcall(function() 
+                shared.Invis(game.Players.LocalPlayer) 
+            end) 
+        end
+    end,
+    HoverText = "invis :joy:"
+})
+
+COB("Utility", {
+    Name = "PingBypass",
+    Function = function(callback) 
+        if callback then
+            pcall(function() 
+                shared.SetPing(game.Players.LocalPlayer, 0) 
+            end) 
+        end
+    end,
+    HoverText = "sets ping to 0 using network functions"
+})
